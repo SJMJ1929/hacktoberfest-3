926a41ebe623ae77dfc454ca36ff5e36870caea2
SJMJ <48629086+SJMJ1929@users.noreply.github.com>
2020-10-02 12:47:10 +0530

    Create maxMin.js

---
+++ maxMin.js
@@ -0,0 +1,6 @@
+function max() 
+{ 
+   let x = document.getElementById("FirstNumber").value 
+   let y = document.getElementById("SecondNumber").value  
+   document.getElementById("result").value = Math.max(x, y);
+} 
