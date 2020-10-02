69520c6f912651d154755f94af8adfc17119acd7
MJ95 <mbhilkar1995@gmail.com>
2020-10-02 13:14:37 +0530

    Update maxMin.js

--- maxMin.js
+++ maxMin.js
@@ -3,4 +3,11 @@ function max()
    let x = document.getElementById("FirstNumber").value 
    let y = document.getElementById("SecondNumber").value  
    document.getElementById("result").value = Math.max(x, y);
+}
+
+function min() 
+{ 
+   let x = document.getElementById("FirstNumber").value 
+   let y = document.getElementById("SecondNumber").value  
+   document.getElementById("result").value = Math.min(x, y);
 } 
